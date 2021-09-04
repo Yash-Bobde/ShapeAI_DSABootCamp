a4f0c15ca93fcac2e00c420a574331fa46e943d6
Yash Bobde <ymbobde99@gmail.com>
2021-09-04 11:08:40 +0530

    Update mainques8.cpp

--- mainques8.cpp
+++ mainques8.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> left_rotation(std::vector<int>& arr, int rotations)
+std::vector<int> left_rot(std::vector<int>& arr, int rot)
 {
     std::vector<int> rotated_array;
 
-    for (std::size_t i = rotations; i < arr.size(); ++i)
+    for (std::size_t i = rot; i < arr.size(); ++i)
     {
         rotated_array.push_back(arr[i]);
     }
-    for (std::size_t i = 0; i < rotations; ++i)
+    for (std::size_t i = 0; i < rot; ++i)
     {
         rotated_array.push_back(arr[i]);
     }
@@ -18,10 +18,10 @@ std::vector<int> left_rotation(std::vector<int>& arr, int rotations)
 
 int main()
 {
-    int num_elements, num_left_rotations;
+    int num_elements, num_left_rot;
 
     std::cin >> num_elements;
-    std::cin >> num_left_rotations;
+    std::cin >> num_left_rot;
 
     std::vector<int> input_array(num_elements);
     for (int i = 0; i < num_elements; ++i)
@@ -29,7 +29,7 @@ int main()
         std::cin >> input_array[i];
     }
 
-    std::vector<int> result_array = left_rotation(input_array, num_left_rotations);
+    std::vector<int> result_array = left_rot(input_array, num_left_rot);
 
     for (int i = 0; i < num_elements; ++i)
     {
